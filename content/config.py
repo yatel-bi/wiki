759ec1752fe7ceb7ab03356f125c85dd49c1b3a2
Juan BC <jbc.develop@gmail.com>
2014-01-15 20:21:58 -0300

    added things

--- content/config.py
+++ content/config.py
@@ -1,15 +1,11 @@
 # encoding: utf-8
+SECRET_KEY='afasdiufhfkhasdnfklhjfgnbdxasdf'
 EXTENSIONS = ['rst2pdf', 'uploads']
 MARKUP = 'restructuredtext'
 PRIVATE = True
 TITLE = 'Yatel Wiki'
 THEME = 'yatel'
 
-
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-del sys
-del os
-
-from lconfig import *
+# please change
+SECRET_KEY = "saknfdlasdhfnseert565876iik0=/(/(%$%#%""#00ljsdsfnjsdnf89"
+CONTENT_DIR = "/home/juan/proyectos/yatel_wiki/content"
